49ba1c8e8b899dc381be2ed9093dc9e9415a1d30
William Mayor <billy@diversityandability.com>
2023-02-03 14:05:58 +0000

    Add option to filter logs by file name

--- tools/restore.py
+++ tools/restore.py
@@ -43,22 +43,25 @@ def rename(log_file, m_time):
     )
 
 
-def main(cutoff):
+def main(cutoff, *search_args):
+    search_args = [a.lower() for a in search_args]
     b = s3.Bucket(config["BACKUP_BUCKET"])
     for days in range(cutoff):
         prefix = date.today() - timedelta(days=days)
         print(f"Looking for files from {prefix}...")
         prefix = str(prefix)
         for obj in b.objects.filter(Prefix=prefix):
-            print(f"  Found {obj.key}")
-            p = Path("logs") / "archive" / obj.key
-            print(f"    Downloading to {p}")
-            with p.open("wb") as fp:
-                b.download_fileobj(obj.key, fp)
+            key = obj.key.lower()
+            if all(a in key for a in search_args):
+                print(f"  Found {obj.key}")
+                p = Path("logs") / "archive" / obj.key
+                print(f"    Downloading to {p}")
+                with p.open("wb") as fp:
+                    b.download_fileobj(obj.key, fp)
 
 
 if __name__ == "__main__":
     cutoff = 7
-    if len(sys.argv) == 2:
+    if len(sys.argv) > 1:
         cutoff = int(sys.argv[1])
-    main(cutoff)
+    main(cutoff, sys.argv[2:])
